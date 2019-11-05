c6803f90436e8c1db3ab2eb49e14b43205b87273
Eric Bach <bach.eric@gmail.com>
2019-11-04 21:29:09 -0700

    Add startup tests

--- Pecuniary.Account.Tests/AccountCommandHandlerTests.cs
+++ Pecuniary.Account.Tests/AccountCommandHandlerTests.cs
@@ -1,15 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Threading;
+using EricBach.CQRS.Commands;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Pecuniary.Account.Command.Controllers;
+using Pecuniary.Account.Data.Commands;
+using Pecuniary.Account.Data.Requests;
+using Pecuniary.Account.Command;
+using Xunit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Pecuniary.Account.Tests
 {
-    class AccountCommandHandlerTests
+    public class AccountCommandHandlerTests
     {
         [Fact]
         public void Test()
         {
 
+        }
+
+        [Fact]
+        public void ConfigureServices_RegistersDependenciesCorrectly()
+        {
+            //  Arrange
+
+            //  Setting up the stuff required for Configuration.GetConnectionString("DefaultConnection")
+            Mock<IConfigurationSection> configurationSectionStub = new Mock<IConfigurationSection>();
+            Mock<Microsoft.Extensions.Configuration.IConfiguration> configurationStub = new Mock<Microsoft.Extensions.Configuration.IConfiguration>();
+            configurationStub.Setup(x => x.GetSection("ConnectionStrings")).Returns(configurationSectionStub.Object);
+
+            IServiceCollection services = new ServiceCollection();
+            var target = new Pecuniary.Account.Command.Startup(configurationStub.Object);
+
+            //  Act
+
+            target.ConfigureServices(services);
+            //  Mimic internal asp.net core logic.
+            services.AddTransient<AccountController>();
+
+            //  Assert
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var controller = serviceProvider.GetService<AccountController>();
+            controller.Should().NotBeNull();
         }
     }
 }
