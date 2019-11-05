03c01a17d6fbee2476655cf78b48439f250a70ef
Eric Bach <bach.eric@gmail.com>
2019-11-04 21:21:00 -0700

    Add tests

---
+++ Pecuniary.Account.Tests/AccountCommandHandlerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pecuniary.Account.Tests
+{
+    class AccountCommandHandlerTests
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
